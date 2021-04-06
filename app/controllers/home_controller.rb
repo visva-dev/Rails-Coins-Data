045fad89bd0683978bd26fc23632a3d589468032
Visvaldas <visva.rapalis@gmail.com>
2021-04-06 12:42:54 +0300

    add controllers currencies lookup in index and lookup pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{Rails.application.credentials.coins[:coins_api_key]}&start=1&limit=5000&convert=USD"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = %w[BTC XRP ADA XLM STEEM]
   end
 
-  def about
-    
+  def about; end
+
+  def lookup
+    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{Rails.application.credentials.coins[:coins_api_key]}&start=1&limit=5000&convert=USD"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+    @symbol = params[:sym]
+    @symbol = @symbol.upcase if @symbol
+    @symbol = 'You forgot to enter a symbol!' if @symbol == ''
   end
 end
